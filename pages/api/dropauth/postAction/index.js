902f71b1d92181bfcab97676b4867a243afffa87
louisdevzz <vohuunhan1310@gmail.com>
2024-02-06 15:22:44 +0700

    fix cors and update action post

---
+++ pages/api/dropauth/postAction/index.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+export default async function handler(req,res){
+    const {method} = req;
+    let client = new MongoClient("mongodb+srv://louisdevzz04:vohuunhan1310@cluster0.zmwbg2i.mongodb.net/dropauth?retryWrites=true&w=majority")
+    let clientPromsie = await client.connect();
+    let db = clientPromsie.db();
+    let col = await db.collection("datas");
+
+    if(method=="POST"){
+        const data = {
+            contentId:req.body.id,
+            postId:req.body.postId,
+            action:req.body.action,
+            userCreated:req.body.userCreated,
+        };
+        try{
+            const result = col.insertOne(data)
+            res.status(200).json(result)
+        }
+        catch(error){
+            res.status(400).json({message: error.message})
+        }
+    }
+}
